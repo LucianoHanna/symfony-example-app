e092da1fb249a9c0afd8dbf609dc6d487bfd5dd9
LucianoHanna <lucianohanna@id.uff.br>
2023-08-23 21:17:50 -0300

    fix: move database migration to dedicated container

---
+++ app/db-migrate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# https://github.com/dunglas/symfony-docker/blob/main/docker/php/docker-entrypoint.sh
+if grep -q ^DATABASE_URL= .env; then
+    echo "Waiting for db to be ready..."
+    ATTEMPTS_LEFT_TO_REACH_DATABASE=60
+    until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(bin/console dbal:run-sql "SELECT 1" 2>&1); do
+        sleep 1
+        ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE - 1))
+        echo "Still waiting for db to be ready... Or maybe the db is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left"
+    done
+
+    if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then
+        echo "The database is not up or not reachable:"
+        echo "$DATABASE_ERROR"
+        exit 1
+    else
+        echo "The db is now ready and reachable"
+    fi
+
+    if [ "$( find ./migrations -iname '*.php' -print -quit )" ]; then
+        php bin/console doctrine:migrations:migrate --no-interaction
+    fi
+fi
+
