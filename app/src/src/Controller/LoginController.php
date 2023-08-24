9c790c9d43cf2a3bcf3f3ae0c272b4bbd02ddd35
LucianoHanna <lucianohanna@id.uff.br>
2023-08-23 23:39:19 -0300

    feat: Add register user back-end

--- app/src/src/Controller/LoginController.php
+++ app/src/src/Controller/LoginController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -27,5 +32,30 @@ class LoginController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/register', name: 'app_register')]
+    public function register(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher, Request $request) : Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('index');
+        }
+        $user = new User;
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            $passwordHash = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($passwordHash);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+        return $this->render('user/register.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
